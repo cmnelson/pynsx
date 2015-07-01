e9e3d2d141c488ca96cbf63268368e4513983590
cmnelson <chad@nelsons.in>
2015-07-01 11:07:41 -0500

    Initial commit

---
+++ pynsx/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pynsx',
+      version='0.1',
+      description='Python for NSX-V',
+      url='https://github.com/cmnelson/pynsx',
+      author='cmnelson',
+      author_email='chad@nelsons.in',
+      license='Apache',
+      packages=['pynsx'],
+      zip_safe=False)
